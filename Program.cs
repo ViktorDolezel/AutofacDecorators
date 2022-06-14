5151bd097f9ce5a4fc7922a4cbb4b731cab976b0
ViktorDolezel <viktor.dolezel@gmail.com>
2022-06-14 13:36:15 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using Autofac;
+using NUnit.Framework;
+using System.Reflection;
+
+[TestFixture]
+public class AutofacTest
+{
+    [Test]
+    public void ResolveAllCommandHandlers_IsSuccessful()
+    {
+        var builder = new ContainerBuilder();
+        builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+            .AsClosedTypesOf(typeof(IHandler<>))
+            .AsImplementedInterfaces();
+
+        var container = builder.Build();
+
+        var commandHandlers = ((IEnumerable<IHandler<Command>>)container.Resolve(typeof(IEnumerable<IHandler<Command>>))).ToList();
+        Assert.AreEqual(commandHandlers[0].GetType(), typeof(CommandHandler1));
+        Assert.AreEqual(commandHandlers[1].GetType(), typeof(CommandHandler2));
+
+    }
+
+    [Test]
+    public void ResolveAllDecoratedCommandHandlers_IsSucessful()
+    {
+        var builder = new ContainerBuilder();
+        builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+            .AsClosedTypesOf(typeof(IHandler<>))
+            .AsImplementedInterfaces();
+
+        builder.RegisterGenericDecorator(
+            typeof(CommandHandlerDecorator<>),
+            typeof(IHandler<>),
+            context => context.ImplementationType.GetInterfaces().Any(t => t.IsGenericType && t.GetGenericTypeDefinition() == typeof(ICommandHandler<>))
+        );
+
+        var container = builder.Build();
+
+        var commandHandlers = ((IEnumerable<IHandler<Command>>)container.Resolve(typeof(IEnumerable<IHandler<Command>>))).ToList();
+        
+        Assert.AreEqual(((CommandHandlerDecorator<Command>)commandHandlers[0]).Decorated.GetType(), typeof(CommandHandler1)); //fails, decorated is typeof(CommandHandler2)
+        Assert.AreEqual(((CommandHandlerDecorator<Command>)commandHandlers[1]).Decorated.GetType(), typeof(CommandHandler2));
+
+    }
+}
+
+
+interface IRequest { }
+interface IHandler<in TRequest> where TRequest : IRequest { public void Handle(TRequest request); } 
+
+interface ICommand: IRequest { }
+interface ICommandHandler<in TCommand>: IHandler<TCommand> where TCommand: ICommand { } 
+
+interface IQuery : IRequest { }
+interface IQueryHandler<in TQuery>: IHandler<TQuery> where TQuery: IQuery { }
+
+class Command : ICommand { }
+class CommandHandler1 : ICommandHandler<Command> { public void Handle(Command command) => Console.WriteLine("CommandHandler1"); }
+class CommandHandler2 : ICommandHandler<Command> { public void Handle(Command command) => Console.WriteLine("CommandHandler2"); }
+
+class Query: IQuery { }
+class QueryHandler1 : IQueryHandler<Query> { public void Handle(Query query) => Console.WriteLine("QueryHandler1"); }
+
+class CommandHandlerDecorator<TCommand> : ICommandHandler<TCommand> where TCommand : ICommand
+{
+    public ICommandHandler<TCommand> Decorated { get; }
+    public CommandHandlerDecorator(ICommandHandler<TCommand> decorated) => Decorated = decorated;
+    public void Handle(TCommand request)
+    {
+        Console.WriteLine($"Command Decorator for {Decorated.GetType().FullName}");
+        Decorated.Handle(request);
+    }
+}
+
+
+
+
+
